4f914714180633916b6f4afd34e62634d4bd5bf4
masmith21 <142959500+masmith21@users.noreply.github.com>
2026-01-11 16:28:23 -0700

    First ALU implementation. Began testbench implementaion
    Co-authored-by: asheryoung10 <asheryoung10@users.noreply.github.com>

--- ALU.v
+++ ALU.v
@@ -1,3 +1,4 @@
+// ALU module that handles opcodes
 module ALU #(parameter BIT_WIDTH    = 16,
              parameter OPCODE_WIDTH =  8,
              parameter FLAG_WIDTH   =  5
@@ -10,169 +11,197 @@ module ALU #(parameter BIT_WIDTH    = 16,
                 output reg [BIT_WIDTH-1:0]    Result 
             );
 
-            localparam ADD    = 8'b0000_0101;
-            localparam ADDI   = 8'0101_xxxx;
-				localparam ADDU   = 8'b0000_0110;
-				localparam ADDUI  = 8'b0110_xxxx;
-				localparam ADDC   = 8'b0000_0111;
-				localparam ADDCI  = 8'b0111_xxxx;
-				localparam SUB    = 8'b0000_1001;
-				localparam SUBI   = 8'b1001_xxxx;
-				localparam CMP 	= 8'b0000_1011;
-				localparam CMPI	= 8'b1011_xxxx;
-				localparam AND 	= 8'b0000_0001;
-				localparam OR 		= 8'b0000_0010;
-				localparam XOR		= 8'b0000_0011;
-				localparam NOT		= 8'b0000_0100;
-				localparam LSH		= 8'b1000_0100;
-				localparam LSHI	= 8'b1000_000x;
-				localparam RSH		= 8'b1000_100x;	
-				localparam RSHI	= 8'b1000_101x;
-				localparam ARSH 	= 8'b1000_0110;
-				localparam ARSHI 	= 8'b1000_001x;
-				localparam NOP		= 8'b0000_0000;
+			// Instruction opcodes
+            localparam ADD    	= 8'b0000_0101;
+            localparam ADDI   	= 8'b0101_xxxx;
+			localparam ADDU   	= 8'b0000_0110;
+			localparam ADDUI  	= 8'b0110_xxxx;
+			localparam ADDC   	= 8'b0000_0111;
+			localparam ADDCI  	= 8'b0111_xxxx;
+			localparam SUB    	= 8'b0000_1001;
+			localparam SUBI   	= 8'b1001_xxxx;
+			localparam CMP 		= 8'b0000_1011;
+			localparam CMPI		= 8'b1011_xxxx;
+			localparam AND 		= 8'b0000_0001;
+			localparam OR 		= 8'b0000_0010;
+			localparam XOR		= 8'b0000_0011;
+			localparam NOT		= 8'b0000_0100;
+			localparam LSH		= 8'b1000_0100;
+			localparam LSHI		= 8'b1000_000x;
+			localparam RSH		= 8'b1000_100x;	
+		 	localparam RSHI		= 8'b1000_101x;
+			localparam ARSH 	= 8'b1000_0110;
+			localparam ARSHI 	= 8'b1000_001x;
+			localparam NOP		= 8'b0000_0000;
 				
-				reg cFlag;
-				reg LFlag;
-				reg fFlag;
-				reg nFlag;
-				reg zFlag;
+			// Individual bit registers to hold flags
+			reg cFlag;
+			reg lFlag;
+			reg fFlag;
+			reg nFlag;
+			reg zFlag;
 				
-				wire [BIT_WIDTH-1:0] shift_amt;
+			// Wire for just holding shift amount for shift instructions.
+			reg [BIT_WIDTH-1:0] shift_amt;
+
+			// Behavioral block for all logic.
             always@(*) begin
-                // Note: In a casex statement you can have opcodes with Don't Cares such as ADDI and also be able
-					 // to set Result or Flags to x as well (i.e. Flags[#] = 1'bx, Result = 16'bx).
-					 
-					 // ALWAYS SET ALL FLAGS AND RESULT OR YOU WILL GET LATCHING!!!!!!
-                
                 casex(Opcode)
+
+						// Signed addition operation.
 						ADD, ADDI: begin
 							Result = $signed(Rdest) + $signed(Rsrc_Imm);
 							
 							zFlag = Result == 0;
 							cFlag = 1'bx;
 							fFlag = (Rdest[15] == Rsrc_Imm[15]) && (Result[15] != Rdest[15]);
-							Lflag = 1'bx;
+							lFlag = 1'bx;
 							nFlag = $signed(Rdest) < $signed(Rsrc_Imm);
 							
-							Flags = {cFlag, LFlag, fFlag, zFlag, nFlag};
-                  end
+							Flags = {cFlag, lFlag, fFlag, zFlag, nFlag};
+						end
+
+						// Unsigned addition operation.
 						ADDU, ADDUI: begin
 							Result = Rdest + Rsrc_Imm;
 							
 							zFlag = Result == 0;
-							cFlag = Result > 2**15;
+							{cFlag, Result} = Rdest + Rsrc_Imm;
 							fFlag = 1'bx;
-							Lflag = Rdest < Rscr_Imm;
+							lFlag = Rdest < Rscr_Imm;
 							nFlag = 1'bx;
 							
-							Flags = {cFlag, LFlag, fFlag, zFlag, nFlag};
+							Flags = {cFlag, lFlag, fFlag, zFlag, nFlag};
 						end
+
+						// Signed addition operation including carry.
 						ADDC, ADDCI: begin
 							Result = cFlag + $signed(Rdest) + $signed(Rsrc_Imm);
 							
 							zFlag = Result == 0;
 							cFlag = 1'bx;
 							fFlag = (Rdest[15] == Rsrc_Imm[15]) && (Result[15] != Rdest[15]);
-							Lflag = 1'bx;
+							lFlag = 1'bx;
 							nFlag = $signed(Rdest) < $signed(Rsrc_Imm);
 							
-							Flags = {cFlag, LFlag, fFlag, zFlag, nFlag};
+							Flags = {cFlag, lFlag, fFlag, zFlag, nFlag};
 						end
+
+						// Signed subtraction operation.
 						SUB, SUBI: begin
-							wire Rsrc2s = ~(Rsrc_Imm) + 1'b1;
-							Result = $signed(Rdest) + $signed(Rsrc2s);
-							
+							reg signed [BIT_WIDTH-1:0] negRsrc;
+							negRsrc = -$signed(Rsrc_Imm);
+							Result = $signed(Rdest) + negRsrc;
+
 							zFlag = Result == 0;
 							cFlag = 1'bx;
 							fFlag = (Rdest[15] == Rsrc_Imm[15]) && (Result[15] != Rdest[15]);
-							Lflag = 1'bx;
+							lFlag = 1'bx;
 							nFlag = $signed(Rdest) < $signed(Rsrc_Imm);
 							
-							Flags = {cFlag, LFlag, fFlag, zFlag, nFlag};
+							Flags = {cFlag, lFlag, fFlag, zFlag, nFlag};
 						end
+
+						// Compare operation.
 						CMP, CMPI: begin
 							Result = 16b'xxxx_xxxx_xxxx_xxxx;
 							zFlag = $signed(Rdest) == $signed(Rsrc_Imm);
 							cFlag = 1'bx;
 							fFlag = 1'bx;
-							Lflag = 1'bx;
+							lFlag = 1'bx;
 							nFlag = $signed(Rdest) < $signed(Rsrc_Imm);
 							
-							Flags = {cFlag, LFlag, fFlag, zFlag, nFlag};
+							Flags = {cFlag, lFlag, fFlag, zFlag, nFlag};
 						end
+
+						// And operation.
 						AND: begin
 							Result = Rdest & Rsrc_Imm;
 							zFlag = Result == 0;
 							cFlag = 1'bx;
 							fFlag = 1'bx;
-							Lflag = 1'bx;
+							lFlag = 1'bx;
 							nFlag = $signed(Rdest) < $signed(Rsrc_Imm);
 							
-							Flags = {cFlag, LFlag, fFlag, zFlag, nFlag};
+							Flags = {cFlag, lFlag, fFlag, zFlag, nFlag};
 						end
+
+						// Or operation.
 						OR: begin
 							Result = Rdest | Rsrc_Imm;
 							ZFlag = Result == 0;
 							cFlag = 1'bx;
 							fFlag = 1'bx;
-							Lflag = 1'bx;
+							lFlag = 1'bx;
 							nFlag = $signed(Rdest) < $signed(Rsrc_Imm);
 							
-							Flags = {cFlag, LFlag, fFlag, zFlag, nFlag};
+							Flags = {cFlag, lFlag, fFlag, zFlag, nFlag};
 						
 						end
+
+						// Xor operation.
 						XOR: begin
 							Result = Rdest ^ Rsrc_Imm;
 							ZFlag = Result == 0;
 							cFlag = 1'bx;
 							fFlag = 1'bx;
-							Lflag = 1'bx;
+							lFlag = 1'bx;
 							nFlag = $signed(Rdest) < $signed(Rsrc_Imm);
 							
-							Flags = {cFlag, LFlag, fFlag, zFlag, nFlag};
+							Flags = {cFlag, lFlag, fFlag, zFlag, nFlag};
 						end
+
+						// Not operation.
 						NOT: begin
 							Result = ~Rsrc_Imm;
 							ZFlag = Result == 0;
 							cFlag = 1'bx;
 							fFlag = 1'bx;
-							Lflag = 1'bx;
+							lFlag = 1'bx;
 							nFlag = $signed(Rdest) < $signed(Rsrc_Imm);
 							
-							Flags = {cFlag, LFlag, fFlag, zFlag, nFlag};
+							Flags = {cFlag, lFlag, fFlag, zFlag, nFlag};
 						end
+
+						// Left shift operation with signed shifting amount.
 						LSH, LSHI: begin
-							shift_amt = $signed(Rsrc_Imm);
-							Result = (shift_amt >= 0) ? ($signed(Rdest) << shift_amt) : ($signed(Rdest) >> -shift_amt);
+							shift_amt = Rsrc_Imm;
+							Result = (shift_amt >= 0) ? (Rdest << shift_amt) : (Rdest >> -shift_amt);
 							
 							ZFlag = Result == 0;
 							cFlag = 1'bx;
 							fFlag = 1'bx;
-							Lflag = 1'bx;
+							lFlag = 1'bx;
 							nFlag = $signed(Rdest) < $signed(Rsrc_Imm);
 							
-							Flags = {cFlag, LFlag, fFlag, zFlag, nFlag};
+							Flags = {cFlag, lFlag, fFlag, zFlag, nFlag};
 						end
 						RSH, RSHI: begin
+							shift_amt = Rsrc_Imm;
+							Result = (shift_amt >= 0) ? (Rdest >> shift_amt) : (Rdest << -shift_amt);
+							
+							ZFlag = Result == 0;
+							cFlag = 1'bx;
+							fFlag = 1'bx;
+							lFlag = 1'bx;
+							nFlag = $signed(Rdest) < $signed(Rsrc_Imm);
+							
+							Flags = {cFlag, lFlag, fFlag, zFlag, nFlag};
+						end
+						ARSH, ARSHI: begin
 							shift_amt = $signed(Rsrc_Imm);
 							Result = (shift_amt >= 0) ? ($signed(Rdest) >> shift_amt) : ($signed(Rdest) << -shift_amt);
 							
 							ZFlag = Result == 0;
 							cFlag = 1'bx;
 							fFlag = 1'bx;
-							Lflag = 1'bx;
+							lFlag = 1'bx;
 							nFlag = $signed(Rdest) < $signed(Rsrc_Imm);
 							
-							Flags = {cFlag, LFlag, fFlag, zFlag, nFlag};
-						end
-						ARSH, ARSHI: begin
-							
-						
+							Flags = {cFlag, lFlag, fFlag, zFlag, nFlag};
 						end
 						NOP: begin
-						
-						
+							Flags = {cFlag, lFlag, fFlag, zFlag, nFlag};
 						end
                 endcase
             end
--- tb_ALU.v
+++ tb_ALU.v
@@ -11,12 +11,37 @@ module tb_ALU;
     wire [FLAG_WIDTH-1:0]    Flags;
     wire [BIT_WIDTH-1:0]    Result; 
 
-    localparam ADD   = 8'b0000_0101;
-    localparam ADDI  = 8'b0101_xxxx;
+    localparam ADD      = 8'b0000_0101;
+    localparam ADDI     = 8'b0101_xxxx;
+    localparam ADDU   	= 8'b0000_0110;
+    localparam ADDUI  	= 8'b0110_xxxx;
+	localparam ADDC   	= 8'b0000_0111;
+	localparam ADDCI  	= 8'b0111_xxxx;
+    localparam SUB    	= 8'b0000_1001;
+	localparam SUBI   	= 8'b1001_xxxx;
+	localparam CMP 		= 8'b0000_1011;
+	localparam CMPI		= 8'b1011_xxxx;
+	localparam AND 		= 8'b0000_0001;
+	localparam OR 		= 8'b0000_0010;
+	localparam XOR		= 8'b0000_0011;
+	localparam NOT		= 8'b0000_0100;
+	localparam LSH		= 8'b1000_0100;
+	localparam LSHI		= 8'b1000_000x;
+	localparam RSH		= 8'b1000_100x;	
+	localparam RSHI		= 8'b1000_101x;
+	localparam ARSH 	= 8'b1000_0110;
+	localparam ARSHI 	= 8'b1000_001x;
+	localparam NOP		= 8'b0000_0000;
+
+    reg [OPCODE_WIDTH-1: 0] opcodes [0:21];
 
     // You can use integers for exhaustive testing though you may have to use a bit mask since your data is 16-bits.
-	 // Look into for-loops and $stop in Verilog if you want to create self-checking testbenches as I demonstrated -- 
-	 // though that is notrequired. Also, don't forget to include just a bit of delay time (#1;) for a display.
+	// Look into for-loops and $stop in Verilog if you want to create self-checking testbenches as I demonstrated -- 
+	// though that is notrequired. Also, don't forget to include just a bit of delay time (#1;) for a display.
+
+    integer i, j, k;
+    wire [FLAG_WIDTH-1:0]   expectedFlags;
+    wire [BIT_WIDTH-1:0]    expectedResult; 
 
     ALU #(.BIT_WIDTH(BIT_WIDTH),
           .OPCODE_WIDTH(OPCODE_WIDTH),
@@ -32,6 +57,40 @@ module tb_ALU;
          );
 
     initial begin
+        opcodes[0] = ADD;
+        opcdoes[1] = ADDI;
+        opcodes[2] = ADDU;
+        opcodes[3] = ADDUI;
+        opcodes[4] = ADDC;
+        opcodes[5] = ADDCI;
+        opcodes[6] = SUB;
+        opcodes[7] = SUBI;
+        opcodes[8] = CMP;
+        opcodes[9] = CMPI;
+        opcodes[10] = AND;
+        opcodes[11] = OR;
+        opcodes[12] = XOR;
+        opcodes[13] = NOT;
+        opcodes[14] = LSH;
+        opcodes[15] = LSHI;
+        opcodes[16] = RSH;
+        opcodes[17] = RSHI;
+        opcodes[18] = ARSH;
+        opcodes[19] = ARSHI;
+        opcodes[20] = NOP;
+
+        $monitor("Rsrc_Imm: %0d, Rdest: %0d, Result: %0d, Flags[1:0]: %b, time:%0d", Rsrc_Imm, Rdest, Result, Flags[15:0], $time);
+        for (i = 0; i < 21 i += 1;) begin
+            Opcode = opcodes[i];
+            for (j = 0; j < 2**16; j += 1;) begin
+                Rsrc_Imm = j;
+                for (k = 0; k < 2**16; k += 1;) begin
+                    Rdest = k;
+                    
+                end
+            end
+        end
+
         #1; $display("ADD TEST"); #1; 
         Opcode = ADD;
 		  Rsrc_Imm = 16'd5;
