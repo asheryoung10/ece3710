c143ea1227cc8c8554660e6e2b90c550476fb535
Asher Young <u1462567@dsl8.cs.utah.edu>
2026-02-26 16:38:13 -0700

    Not done yet sad

--- Verilog/Audio Test/tb_i2c_audio_setup.v
+++ Verilog/Audio Test/tb_i2c_audio_setup.v
@@ -4,28 +4,28 @@ module tb_i2c_audio_setup;
     reg fpga50MHzClock;
     reg reset;
     reg start;
-    reg [7:0] address;
-    reg [7:0] firstByte;
-    reg [7:0] secondByte;
     wire busy;
     wire done;
     wire error;
     wire i2c_clock;
     wire i2c_data;
+	 wire [7:0] debugAddress;
+	 wire [7:0] debugFirstByte;
+	 wire [7:0] debugSecondByte;
 
     // Instantiate the I2C module
-    i2c_audio_setup uut (
+    i2c_commands uut (
         .fpga50MHzClock(fpga50MHzClock),
         .reset(reset),
         .start(start),
-        .address(address),
-        .firstByte(firstByte),
-        .secondByte(secondByte),
         .busy(busy),
         .done(done),
         .error(error),
         .i2c_clock(i2c_clock),
-        .i2c_data(i2c_data)
+        .i2c_data(i2c_data),
+		  .debugAddress(debugAddress),
+		  .debugFirstByte(debugFirstByte),
+		  .debugSecondByte(debugSecondByte)
     );
 
     // Generate the 50 MHz FPGA clock
@@ -35,15 +35,21 @@ module tb_i2c_audio_setup;
         fpga50MHzClock = 1'b1;
         #10;
     end
+	 
+	 pullup(i2c_clock);
+	 pullup(i2c_data);
 
+	     // Monitor the signals for debugging
+    initial begin
+        $monitor("Time = %t, i2c_clock = %b, i2c_clock_value = %b, i2c_data = %b, i2c_data_value = %b, busy = %b, done = %b, error = %b, state = %b, stepIndex = %b", 
+                 $time, i2c_clock, uut.i2c_instance.i2c_clock_value, i2c_data, uut.i2c_instance.i2c_clock_value, busy, done, error, uut.i2c_instance.state, uut.i2c_instance.subStep);
+    end
+	 
     // Stimulus
     initial begin
         // Initialize signals
         reset = 0;
         start = 0;
-        address = 8'hA0;   // Example address (I2C write address)
-        firstByte = 8'h55;  // Example first byte to send
-        secondByte = 8'hAA; // Example second byte to send
         
         // Reset the system
         reset = 1;
@@ -51,22 +57,18 @@ module tb_i2c_audio_setup;
         reset = 0;
         
         // Start the I2C communication
-        #20;
+        #40;
         start = 1; // Assert start signal
         #20;
         start = 0; // Deassert start signal
         
         // Wait some time to observe the communication
-        #200;
+        #10000;
 
         // Finish the simulation
         $stop;
     end
 
-    // Monitor the signals for debugging
-    initial begin
-        $monitor("Time = %t, i2c_clock = %b, i2c_data = %b, busy = %b, done = %b, error = %b", 
-                 $time, i2c_clock, i2c_data, busy, done, error);
-    end
+
 
 endmodule
