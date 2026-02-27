8aaa84427843ae26197444252f7b0472342a9128
Asher Young <u1462567@dsl8.cs.utah.edu>
2026-02-27 10:21:41 -0700

    More audio testing

--- Verilog/Audio Test 2/top.v
+++ Verilog/Audio Test 2/top.v
@@ -3,6 +3,8 @@ module top(
     input  RESET_N,      // Active-low reset (usually a pushbutton)
     input configure_N,
 	 input wire [1:0] pitch,
+	 input wire enableOutput,
+	 input wire [3:0] selectedCommand,
     // I2C Pins
     inout  FPGA_I2C_SCLK,
     inout  FPGA_I2C_SDAT,
@@ -12,29 +14,32 @@ module top(
     output AUD_BCLK,     // Bit Clock
     output AUD_DACLRCK,  // DAC Sample Clock
     output AUD_DACDAT,    // DAC Serial Data
-	 output error
+	 output error,
+	 output wire busy,
+	 output wire done
 );
 
-    wire config_done;
-    wire config_busy;
+
 
     // 1. Instance: Audio Configuration (I2C)
     // This talks to the chip registers to unmute and power it up.
-    audio_configurator i2c_conf (
+    audio_configurator_select i2c_conf (
         .systemClock(CLOCK_50),
         .reset(~RESET_N),        // Converting active-low to active-high
         .configure(~configure_N),        // Always try to configure on power-up
-        .busy(config_busy),
-        .done(config_done),
+		  .selectedCommand(selectedCommand),
+        .busy(busy),
+        .done(done),
         .encounteredError(error),     // Unused for this simple setup
         .i2c_clock(FPGA_I2C_SCLK),
-        .i2c_data(FPGA_I2C_SDAT)
+        .i2c_data(FPGA_I2C_SDAT),
+
     );
 
     // 2. Instance: Audio Output Logic (I2S)
     // This generates the actual square wave bitstream.
     audio_output dac_driver (
-        .clk_50(CLOCK_50),
+        .clk_50(CLOCK_50 & enableOutput),
 		  .pitch_sel(pitch),
         .aud_xck(AUD_XCK),
         .aud_bclk(AUD_BCLK),
