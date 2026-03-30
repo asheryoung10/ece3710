5701903f4ffccbdc1c3df194b8dcbe07f462c05f
parkerrowland3 <parkerrowland3@gmail.com>
2026-03-30 12:30:55 -0600

    Add multiline and label functionality

--- Assembler/program.py
+++ Assembler/program.py
@@ -1,20 +1,25 @@
-from assembler import assembleInstruction
+from assembler import assembleProgram
 
 
-print(assembleInstruction("MOVI 0x01, R0"));
-print(assembleInstruction("LSHI 15, R0"));
-print(assembleInstruction("MOVI 100, R1"));
-print(assembleInstruction("STOR R1, R0"));
+program_source = """
+MOVI 0x01, R0
+LSHI 15, R0
+MOVI 100, R1
+STOR R1, R0
 
-print(assembleInstruction("MOVI 0x01, R0"));
-print(assembleInstruction("LSHI 14, R0"));
-print(assembleInstruction("MOVI 100, R1"));
-print(assembleInstruction("STOR R1, R0"));
+MOVI 0x01, R0
+LSHI 14, R0
+MOVI 100, R1
+STOR R1, R0
 
-print(assembleInstruction("MOVI 0x01, R0"));
-print(assembleInstruction("MOVI 0x01, R2"));
-print(assembleInstruction("LSHI 14, R0"));
-print(assembleInstruction("OR R2, R0"));
-print(assembleInstruction("MOVI 100, R1"));
-print(assembleInstruction("STOR R1, R0"));
+MOVI 0x01, R0
+MOVI 0x01, R2
+LSHI 14, R0
+OR R2, R0
+MOVI 100, R1
+STOR R1, R0
+WAIT
+"""
 
+
+print(assembleProgram(program_source))
