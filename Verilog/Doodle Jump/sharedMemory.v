f6c83fe9d465e64ab3409297a6f39c44cc996340
Asher Young <u1462567@dsl7.cs.utah.edu>
2026-04-04 23:04:24 -0600

    Readded background and rectangle rending that is doodle jump like. Added collisions in software

--- Verilog/Doodle Jump/sharedMemory.v
+++ Verilog/Doodle Jump/sharedMemory.v
@@ -95,36 +95,113 @@ end
 // Draw Rectangles
 reg [7:0] width;
 reg [7:0] height;
-
 reg [15:0] x;
 reg [15:0] y;
 reg [15:0] wh;
 reg [15:0] color;
+reg [7:0] base_r;
+reg [7:0] base_g;
+reg [7:0] base_b;
+reg inside_platform;
+reg inside_rounded_shape;
+reg cut_corner;
 integer i;
+integer edge_dx;
+integer edge_dy;
+ 
+localparam integer CORNER_RADIUS = 3;
+localparam [15:0] DOODLE_GREEN = 16'h3666; // soft green fill
+ 
 always @(*) begin
-
+    // Transparent/black default so the background layer can show through.
     r = 8'h00;
     g = 8'h00;
     b = 8'h00;
-
+ 
     for (i = 0; i < 16; i = i + 1) begin
         x     = rect_data[i*4 + 0];
         y     = rect_data[i*4 + 1];
         wh    = rect_data[i*4 + 2];
         color = rect_data[i*4 + 3];
-
+ 
         width  = wh[15:8];
         height = wh[7:0];
-
-        if (
+ 
+        // Use the stored color if the CPU writes one, otherwise default to a Doodle-style green.
+        if (color == 16'h0000) begin
+            base_r = {DOODLE_GREEN[15:11], 3'b000};
+            base_g = {DOODLE_GREEN[10:5],  2'b00};
+            base_b = {DOODLE_GREEN[4:0],   3'b000};
+        end else begin
+            base_r = {color[15:11], 3'b000};
+            base_g = {color[10:5],  2'b00};
+            base_b = {color[4:0],   3'b000};
+        end
+ 
+        inside_platform =
             (vgaX >= x) &&
             (vgaX < x + width) &&
             (vgaY >= y) &&
-            (vgaY < y + height)
-        ) begin
-            r = {color[15:11], 3'b000};
-            g = {color[10:5],  2'b00};
-            b = {color[4:0],   3'b000};
+            (vgaY < y + height);
+ 
+        cut_corner = 1'b0;
+ 
+        if (inside_platform && (width > (CORNER_RADIUS * 2)) && (height > (CORNER_RADIUS * 2))) begin
+            // Cheap FPGA-friendly rounded corners using simple add/compare logic.
+            // Top-left corner
+            if ((vgaX < x + CORNER_RADIUS) && (vgaY < y + CORNER_RADIUS)) begin
+                edge_dx = vgaX - x;
+                edge_dy = vgaY - y;
+                if ((edge_dx + edge_dy) < (CORNER_RADIUS - 1))
+                    cut_corner = 1'b1;
+            end
+ 
+            // Top-right corner
+            if ((vgaX >= x + width - CORNER_RADIUS) && (vgaY < y + CORNER_RADIUS)) begin
+                edge_dx = (x + width - 1) - vgaX;
+                edge_dy = vgaY - y;
+                if ((edge_dx + edge_dy) < (CORNER_RADIUS - 1))
+                    cut_corner = 1'b1;
+            end
+ 
+            // Bottom-left corner
+            if ((vgaX < x + CORNER_RADIUS) && (vgaY >= y + height - CORNER_RADIUS)) begin
+                edge_dx = vgaX - x;
+                edge_dy = (y + height - 1) - vgaY;
+                if ((edge_dx + edge_dy) < (CORNER_RADIUS - 1))
+                    cut_corner = 1'b1;
+            end
+ 
+            // Bottom-right corner
+            if ((vgaX >= x + width - CORNER_RADIUS) && (vgaY >= y + height - CORNER_RADIUS)) begin
+                edge_dx = (x + width - 1) - vgaX;
+                edge_dy = (y + height - 1) - vgaY;
+                if ((edge_dx + edge_dy) < (CORNER_RADIUS - 1))
+                    cut_corner = 1'b1;
+            end
+        end
+ 
+        inside_rounded_shape = inside_platform && !cut_corner;
+ 
+        if (inside_rounded_shape) begin
+            // Darker top band to make the platform read more like Doodle Jump.
+            if (vgaY < y + 3) begin
+                r = base_r >> 1;
+                g = (base_g >> 1) + 8'd40;
+                b = base_b >> 1;
+            end
+            // Slight darker bottom edge for depth.
+            else if (vgaY >= y + height - 2) begin
+                r = base_r >> 1;
+                g = base_g >> 1;
+                b = base_b >> 1;
+            end
+            // Main fill.
+            else begin
+                r = base_r;
+                g = base_g;
+                b = base_b;
+            end
         end
     end
 end
