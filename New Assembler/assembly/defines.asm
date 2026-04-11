8128906711c7835ca4aa1537a8cdfd3cbcce9092
asheryoung10 <asheryoung10@gmail.com>
2026-04-10 18:08:10 -0600

    need to fix assembler

---
+++ New Assembler/assembly/defines.asm
@@ -0,0 +1,48 @@
+#define constPlayer1XAddress() 0xC000
+#define constPlayer1YAddress() 0xC001
+#define constPlayer1AnimationIndexAddress() 0xC002
+#define constPlayer1HighlightColorAddress() 0xC003
+
+#define constPlayer2XAddress() 0xC004
+#define constPlayer2YAddress() 0xC005
+#define constPlayer2AnimationIndexAddress() 0xC006
+#define constPlayer2HighlightColorAddress() 0xC007
+
+#define constPlayer3XAddress() 0xC008
+#define constPlayer3YAddress() 0xC009
+#define constPlayer3AnimationIndexAddress() 0xC00A
+#define constPlayer3HighlightColorAddress() 0xC00B
+
+#define constPlayer4XAddress() 0xC00C
+#define constPlayer4YAddress() 0xC00D
+#define constPlayer4AnimationIndexAddress() 0xC00E
+#define constPlayer4HighlightColorAddress() 0xC00F
+
+#define constPlayerWidth() 45
+#define constPlayerHeight() 45
+
+#define constPlayer1LeftMask() 0x0001
+#define constPlayer1RightMask() 0x0002
+#define constPlayer1UpMask() 0x0004
+#define constPlayer1DownMask() 0x0008
+#define constPlayer1Mask() 0x000F
+
+#define constPlayer2LeftMask() 0x0010
+#define constPlayer2RightMask() 0x0020
+#define constPlayer2UpMask() 0x0040
+#define constPlayer2DownMask() 0x0080
+#define constPlayer2Mask() 0x00F0
+
+#define constPlayer3LeftMask() 0x0100
+#define constPlayer3RightMask() 0x0200
+#define constPlayer3UpMask() 0x0400
+#define constPlayer3DownMask() 0x0800
+#define constPlayer3Mask() 0x0F00
+
+#define constPlayer4LeftMask() 0x1000
+#define constPlayer4RightMask() 0x2000
+#define constPlayer4UpMask() 0x4000
+#define constPlayer4DownMask() 0x8000
+#define constPlayer4Mask() 0xF000
+
+#define constGreatestCPUAddress() 0x8095
