8868709bd12e7b12b50e9179a4d435d94c5684c7
asheryoung10 <144182798+asheryoung10@users.noreply.github.com>
2026-04-11 22:37:16 -0600

    Implemented somewhat of a lobby, but not near complete

--- New Assembler/assembly/sendPlayerPositions.asm
+++ New Assembler/assembly/sendPlayerPositions.asm
@@ -1,33 +1,3 @@
-funcSendPlayerPositions:
-
-    MOVI 0 R0                            // player index
-    READ R12 | &varActivePlayersAddress  // active player list
-    READ R2  | &varPlayerPhysicsDataAddress     // player physics list
-    READ R3  | %playerSharedDataAddress  // player draw list
-
-    funcSendPlayerPositionsPlayerLoop:
-
-        LOAD R4 R2              // R4 = player X pos
-        ADDI 1 R2 | LOAD R5 R2  // R5 = player Y pos
-
-        LOAD R10 R12 
-        CMPI 0 R10 | GOIF NE &funcSendPlayerPositionsPlayerLoopDrawOffscreen    // 
-        MOVI -64 R4 | MOVI -64 R5                                               // Put data to send in undrawable region.
-        funcSendPlayerPositionsPlayerLoopDrawOffscreen:                         //
-
-        STOR R4 R3 // Store x
-        ADDI 1 R3 | STOR R5 R3 // Store y
-
-        ADDI 3 R2 // next player in physics list
-        ADDI 3 R3 // next player in draw list
-        ADDI 1 R0 // i++
-        ADDI 1 R12 // next active player
-        CMPI 4 R0
-        GOIF NE &funcSendPlayerPositionsPlayerLoop 
-
-    %return
-
-
 funcJoinLeavePlayers:
     %save(R6)
     %save(R7)
@@ -36,21 +6,20 @@ funcJoinLeavePlayers:
 
     MOVI 0 R0 // Player index
     MOVI 8 R1 // Current player 1 down mask
-    READ R2 | &varPreviousButtonStateAddress | LOAD R2 R2
     READ R4 | &varActivePlayersAddress
-    READ R5 | &varPlayerPhysicsDataAddress
+    READ R5 | %playerSharedDataAddress
     READ R6 | 215  // Start x pos
     READ R7 | 220 // y pos
+    READ R8 | &varPlayerDefaultColors
+    READ R9 | %playerSharedDataAddress
+    ADDI 3 R9 // Color
     
     funcJoinLeavePlayersPlayerLoop:
         
         MOV R1 R3 // R3 = player down mask
-        AND R2 R3 // R3 = 1 if down was pressed last frame
-        CMPI 0 R3 | GOIF NE &funcJoinLeavePlayersPlayerLoopNext // if pressed last frame then we dont want to do anything
-
-        MOV R1 R3 // R3 = player down mask
-        AND R15 R3 // R3 = 1 if down was pressed this frame
-        CMPI 0 R3 | GOIF EQ &funcJoinLeavePlayersPlayerLoopNext // if they didn't press the button dont do anything
+        READ R12  | &varButtonPressedThisFrameAddress | LOAD R12 R12
+        AND R12 R3
+        CMPI 0 R3 | GOIF EQ &funcJoinLeavePlayersPlayerLoopNext //if not pressed this frame do nothing
 
         LOAD R12 R4
         NOT R12 R12
@@ -61,9 +30,19 @@ funcJoinLeavePlayers:
         STOR R7 R5
         SUBI 1 R5
 
+        READ R10 | 0x0000 // black color if inactive
+
+        CMPI 0 R12 | GOIF EQ &funcJoinLeavePlayersPlayerLoopSetColorInactiveSkip
+        LOAD R12 R8
+
+
+        funcJoinLeavePlayersPlayerLoopSetColorInactiveSkip:
+        STOR R12 R9
 
         funcJoinLeavePlayersPlayerLoopNext:
+        ADDI 1 R8
         ADDI 4 R5
+        ADDI 4 R9
         ADDI 1 R0
         ADDI 55 R6 // Space players out
         ADDI 1 R4
@@ -76,3 +55,117 @@ funcJoinLeavePlayers:
     %restore(R7)
     %restore(R6)
     %return
+
+// Takes in R0 the address of player data to copy
+funcCopyPlayerData:
+    MOVI 0 R1 // i = 0
+    READ R2 | %playerSharedDataAddress
+
+    funcCopyPlayerDataLoop:
+
+        LOAD R3 R0
+        STOR R3 R2
+
+        ADDI 1 R2
+        ADDI 1 R0
+        ADDI 1 R1 // i++
+        CMPI 16 R1
+        GOIF NE &funcCopyPlayerDataLoop
+    %return
+
+funcApplyUserInput:
+
+    MOVI 0 R0 // i = 0
+    MOVI 1 R1 // player input mask
+    READ R2 | &varPlayerVelocityAddress
+    READ R3 | &varActivePlayersAddress
+    READ R4 | %playerSharedDataAddress
+
+    funcApplyUserInputPlayerLoop:
+        LOAD R5 R3 | CMPI 0 R5 | GOIF EQ &funcApplyUserInputPlayerLoopContinue // Skip player if inactive
+
+        MOVI 0 R8 // Detects if a horizontal input
+        MOVI 0 R6
+        MOV R1 R5 | AND R15 R5 | CMPI 0 R5 | GOIF EQ &funcApplyUserInputSkipLeft
+        ADDI -2 R6 // Amount to add
+        MOVI 1 R8
+        funcApplyUserInputSkipLeft:
+        LOAD R7 R2 | ADD R6 R7
+        CMPI %minVelocityX R7 | GOIF LE &funcApplyUserInputSkipClampLeft
+        MOVI %minVelocityX R7
+        funcApplyUserInputSkipClampLeft:
+        STOR R7 R2
+
+        LSHI 1 R1
+
+        MOVI 0 R6
+        MOV R1 R5 | AND R15 R5 | CMPI 0 R5 | GOIF EQ &funcApplyUserInputSkipRight
+        ADDI 2 R6 // Amount to add
+        MOVI 1 R8
+        funcApplyUserInputSkipRight:
+        LOAD R7 R2 | ADD R6 R7
+        CMPI %maxVelocityX R7 | GOIF GE &funcApplyUserInputSkipClampRight
+        MOVI %maxVelocityX R7
+        funcApplyUserInputSkipClampRight:
+        STOR R7 R2
+
+        CMPI 1 R8 | GOIF EQ &funcApplyUserInputSkipHorizontalFriction
+
+        LOAD R7 R2 // R7 = velocityx
+        CMPI 0 R7 | GOIF EQ &funcApplyUserInputSkipHorizontalFriction
+        MOVI 1 R8
+        CMPI 0 R7 | GOIF GE &funcApplyUserInputHoritonalFrictionInternalSkip
+        MOVI -1 R8
+        funcApplyUserInputHoritonalFrictionInternalSkip:
+        ADD R8 R7
+        STOR R7 R2
+        funcApplyUserInputSkipHorizontalFriction:
+        LSHI -1 R1
+
+        funcApplyUserInputPlayerLoopContinue:
+        ADDI 2 R2 // step player velocity
+        ADDI 1 R3 // step active player
+        ADDI 4 R4 // Step shared player
+        LSHI 4 R1 // Step input mask
+        ADDI 1 R0 | CMPI 4 R0 | GOIF NE &funcApplyUserInputPlayerLoop
+
+    %return
+
+funcApplyVelocity:
+
+    MOVI 0 R0 // i = 0
+    READ R2 | &varPlayerVelocityAddress
+    READ R3 | &varActivePlayersAddress
+    READ R4 | %playerSharedDataAddress
+
+    funcApplyVelocityPlayerLoop:
+        LOAD R5 R3 | CMPI 0 R5 | GOIF EQ &funcApplyVelocityPlayerLoopContinue // Skip player if inactive
+
+        LOAD R1 R4 // R1 = positionx
+        LOAD R5 R2 // R5 = velocityx
+        ARSHI -2 R5 // Remove lower bits of velocity
+
+        ADD R5 R1  // Apply velocity
+        STOR R1 R4 // Store new position
+
+        ADDI 1 R4
+        ADDI 1 R2
+
+        LOAD R1 R4 // R1 = positiony
+        LOAD R5 R2 // R5 = velocityy
+        ARSHI -2 R5 // Remove lower bits of velocity
+
+        ADD R5 R1  // Apply velocity
+        STOR R1 R4 // Store new position
+
+        SUBI 1 R2
+        SUBI 1 R4
+
+        funcApplyVelocityPlayerLoopContinue:
+        ADDI 2 R2 // step player velocity
+        ADDI 1 R3 // step active player
+        ADDI 4 R4 // Step shared player
+        ADDI 1 R0 | CMPI 4 R0 | GOIF NE &funcApplyVelocityPlayerLoop
+
+    %return
+
