b9597ac98b92e3da4dc11ef7a102b0eebd34bff9
asheryoung10 <asheryoung10@gmail.com>
2026-04-13 16:41:39 -0600

    reduce file

--- Simulator/build/Desktop_Qt_6_10_2_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Simulator_FILE C:/Users/asher/Desktop/SchoolWork/ece3710/Simulator/build/Desktop_Qt_6_10_2_MinGW_64_bit-Release/Simulator.exe)
-set(__QT_DEPLOY_TARGET_Simulator_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Simulator_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
