a948bbf954dd6b48b30ba68670dc9050cb422f58
Maxwell Smith <u1397073@utah.edu>
2026-04-14 16:42:45 -0600

    fixed double jump off rectangles

--- New Assembler/assembly/detectCollision.asm
+++ New Assembler/assembly/detectCollision.asm
@@ -15,7 +15,12 @@ detectAndResolveCollisions:
             %restoreReg(R1)
             CMPI 0 R0 | GOIF EQ &detectAndResolveCollisionsRectLoopContinue
 
-            // Collision detected, jump player 
+            // Only collide if player y velocity is negative
+            MOV R3 R4 // R4 has player velocity address y
+            LOAD R4 R4
+            CMPI 0 R4 | GOIF GT &detectAndResolveCollisionsRectLoopContinue
+
+            // Collision detected, jump player
             MOVI 0 R4
             SUBI %playerJumpIncrement R4
             STOR R4 R3
---
+++ Simulator/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/CMakeFiles/Simulator.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Simulator_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Simulator_autogen.dir/ParseCache.txt"
+  "Simulator_autogen"
+  "CMakeFiles/Simulator.dir/Simulator_autogen/EWIEGA46WW/qrc_resource.cpp.obj"
+  "CMakeFiles/Simulator.dir/Simulator_autogen/EWIEGA46WW/qrc_resource.cpp.obj.d"
+  "CMakeFiles/Simulator.dir/Simulator_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/Simulator.dir/Simulator_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/Simulator.dir/main.cpp.obj"
+  "CMakeFiles/Simulator.dir/main.cpp.obj.d"
+  "CMakeFiles/Simulator.dir/mainwindow.cpp.obj"
+  "CMakeFiles/Simulator.dir/mainwindow.cpp.obj.d"
+  "CMakeFiles/Simulator.dir/model.cpp.obj"
+  "CMakeFiles/Simulator.dir/model.cpp.obj.d"
+  "CMakeFiles/Simulator.dir/vgaview.cpp.obj"
+  "CMakeFiles/Simulator.dir/vgaview.cpp.obj.d"
+  "Simulator.exe"
+  "Simulator.exe.manifest"
+  "Simulator.pdb"
+  "Simulator_autogen/EWIEGA46WW/qrc_resource.cpp"
+  "Simulator_autogen/mocs_compilation.cpp"
+  "Simulator_autogen/timestamp"
+  "libSimulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
